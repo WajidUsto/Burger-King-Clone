86308ca9541f8c1b190c6bee962e268ee668dcb5
WajidUsto <wajidusto@gmail.com>
2024-09-02 01:07:30 +0500

    command added

--- app.js
+++ app.js
@@ -1,50 +1,111 @@
 // Gsap Animation
 
+
+
+// var tl = gsap.timeline()
+// tl.from("#heading-text",{
+//     y:20,
+//     opacity:0,
+//     duration:2,
+//     delay:2
+// })
+// tl.from("#heading-text h1",{
+//     y:20,
+//     duration:2,
+//     opacity:0,
+//     stagger:0.3,
+//     delay:2
+// })
+// tl.from("heading-text main-btn-1",{
+//         y:-20,
+//         duration:2,
+//         opacity:0,
+//         stagger:0.3,
+//         delay:2
+//     })
+    // var tl = gsap.timeline()
+    // // tl.from("#heading-text",{
+    // //     y:-20,
+    // //     opacity:0,
+    // //     duration:2,
+    // //     delay:1,
+    // // })
+    
+    // tl.from("#heading-text h1",{
+    //     y:-50,
+    //     duration:1,
+    //     opacity:0,
+    //     stagger:0.5,
+    // })
+    // tl.from("#main-btn-1",{
+    //     y:50,
+    //     duration:1,
+    //     opacity:0,
+    //     // stagger:0.5,
+    // })
+    // tl.from("#main-btn-2",{
+    //     y:50,
+    //     duration:1,
+    //     opacity:0,
+    //     // stagger:0.5,
+    // })
 // Scrolling Animation Started
 // Scrolling Animation Started
 
+
+
 gsap.from("#I1",{
-    duration:0.3,
+    duration:0.4,
     opacity:0,
     y:200,
     scrollTrigger:{
         trigger:"#I1",
         scroller:"body",
-        markers:true,
         start:"top 65%",
     }
 })
 gsap.from("#I2",{
-    duration:0.3,
+    duration:0.4,
     opacity:0,
     y:200,
     scrollTrigger:{
         trigger:"#I2",
         scroller:"body",
-        markers:true,
         start:"top 60%",
     }
 })
 gsap.from("#I3",{
-    duration:0.3,
+    duration:0.4,
     opacity:0,
-    y:500,
+    y:300,
     scrollTrigger:{
         trigger:"#I1",
         scroller:"body",
-        markers:true,
         start:"bottom 45%",
     }
 })
 gsap.from("#I4",{
-    duration:0.3,
+    duration:0.4,
     opacity:0,
-    y:500,
+    y:300,
     scrollTrigger:{
         trigger:"#I2",
         scroller:"body",
-        markers:true,
         start:"bottom 40%",
+    }
+})
+// Phone Section Scrolling
+// Phone Section Scrolling
+gsap.from("#phone-section-inside",{
+    duration:0.4,
+    opacity:0,
+    y:100,
+    scrollTrigger:{
+        trigger:"phone-section-inside",
+        scroller:"body",
+        markers:true,
+        start:"top 50%",
+        end:"bottom 50%",
     }
 })
 // gsap.from("#cards-row-2",{
